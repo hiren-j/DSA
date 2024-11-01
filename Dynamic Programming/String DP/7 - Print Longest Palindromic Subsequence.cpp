218d6f815d6de967879e659222eb3d525b322eaf
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-11-01 10:02:53 +0530

    Update 7 - Print Longest Palindromic Subsequence.cpp

--- Dynamic Programming/String DP/7 - Print Longest Palindromic Subsequence.cpp
+++ Dynamic Programming/String DP/7 - Print Longest Palindromic Subsequence.cpp
@@ -2,7 +2,6 @@
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Class to implement the Bottom-up approach:
 class BottomUp {
 public:
     // Method to find the LPS of given string, using 2D tabulation - O(N*M) & O(N*M)
