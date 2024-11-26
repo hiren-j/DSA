fce46e9a047377e15a39ebb0d40f1777389b8923
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-11-26 19:02:04 +0530

    Update 6 - Minimum Cost Climbing Stairs.cpp

--- Dynamic Programming/1D DP/6 - Minimum Cost Climbing Stairs.cpp
+++ Dynamic Programming/1D DP/6 - Minimum Cost Climbing Stairs.cpp
@@ -2,7 +2,6 @@
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Class to implement the Top-down approach:
 class TopDown {
 public:
     // Method to find the minimum cost, using recursion with memoization - O(N) & O(N)
@@ -55,7 +54,6 @@ private:
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Class to implement the Bottom-up approach:
 class BottomUp {
 public:
     // #1 Method to find the minimum cost, using 1D tabulation - O(N) & O(N)
