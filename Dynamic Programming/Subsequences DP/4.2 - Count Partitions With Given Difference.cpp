ec93a54935cbf8e4b0916a11f59ea792306135e2
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-01-08 17:43:01 +0530

    Create 4.2 - Count Partitions With Given Difference.cpp

---
+++ Dynamic Programming/Subsequences DP/4.2 - Count Partitions With Given Difference.cpp
@@ -0,0 +1,108 @@
+class TopDown {
+    int solveWithMemo(vector<vector<int>>& dp, vector<int>& nums, int index, int target) {
+        if(index == 0) {
+            if(target == 0) 
+                return (nums[0] == 0) ? 2 : 1;
+            else
+                return (nums[0] == target);
+        }
+
+        if(target < 0)
+            return 0;
+
+        if(dp[index][target] != -1)
+            return dp[index][target];
+
+        int currSkip = solveWithMemo(dp, nums, index - 1, target);
+        int currTake = solveWithMemo(dp, nums, index - 1, target - nums[index]);
+
+        return dp[index][target] = (currSkip + currTake);
+    }
+
+public:
+    // O(N*TS) & O(N*TS) : Where TS = totalSum
+    int countPartitions(vector<int>& nums, int difference) {
+        int n = nums.size();
+        int resCount = 0;
+        int totalSum = accumulate(begin(nums), end(nums), 0);
+
+        vector<vector<int>> dp(n, vector<int>(totalSum + 1, -1));
+
+        for(int subset1Sum = 0; subset1Sum <= totalSum; ++subset1Sum) {
+            int currCount  = solveWithMemo(dp, nums, n-1, subset1Sum);
+            int subset2Sum = totalSum - subset1Sum;
+            if(subset1Sum >= subset2Sum && subset1Sum - subset2Sum == difference) {
+                resCount += currCount;
+            }
+        }
+
+        return resCount;
+    }
+};
+
+class BottomUp {
+public:
+    // O(N*TS) & O(N*TS)
+    int countPartitions_V1(vector<int>& nums, int difference) {
+        int n = nums.size();
+        int totalSum = accumulate(begin(nums), end(nums), 0);
+
+        vector<vector<int>> dp(n, vector<int>(totalSum + 1, 0));
+        dp[0][0] = 1;
+        if(nums[0] == 0) dp[0][0] = 2;
+        else if(nums[0] <= totalSum) dp[0][nums[0]] = 1;
+
+        for(int index = 1; index < n; ++index) {
+            for(int target = 0; target <= totalSum; ++target) {
+                int currSkip = dp[index - 1][target];
+                int currTake = (target - nums[index] >= 0) ? dp[index - 1][target - nums[index]] : 0;
+                dp[index][target] = (currSkip + currTake);
+            }
+        }
+
+        int resCount = 0;
+
+        for(int subset1Sum = 0; subset1Sum <= totalSum; ++subset1Sum) {
+            int subset2Sum = totalSum - subset1Sum;
+            if(subset1Sum >= subset2Sum && subset1Sum - subset2Sum == difference) {
+                resCount += dp[n-1][subset1Sum];
+            }
+        }
+
+        return resCount;
+    }
+
+    // O(N*TS) & O(TS)
+    int countPartitions_V2(vector<int>& nums, int difference) {
+        int n = nums.size();
+        int totalSum = accumulate(begin(nums), end(nums), 0);
+
+        vector<int> prevRow(totalSum + 1, 0), idealRow(totalSum + 1, 0);
+        prevRow[0] = 1;
+        if(nums[0] == 0) prevRow[0] = 2;
+        else if(nums[0] <= totalSum) prevRow[nums[0]] = 1;
+
+        for(int index = 1; index < n; ++index) {
+            for(int target = 0; target <= totalSum; ++target) {
+                int currSkip = prevRow[target];
+                int currTake = (target - nums[index] >= 0) ? prevRow[target - nums[index]] : 0;
+                idealRow[target] = (currSkip + currTake);
+            }
+            prevRow = idealRow;
+        }
+
+        int resCount = 0;
+
+        for(int subset1Sum = 0; subset1Sum <= totalSum; ++subset1Sum) {
+            int subset2Sum = totalSum - subset1Sum;
+            if(subset1Sum >= subset2Sum && subset1Sum - subset2Sum == difference) {
+                resCount += prevRow[subset1Sum];
+            }
+        }
+
+        return resCount;
+    }
+};
+
+Topics: Array | Dynamic Programming
+Link  : https://www.geeksforgeeks.org/problems/partitions-with-given-difference/1
