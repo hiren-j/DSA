237c829114ef6072238457e791fd386c821bfd5a
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-01-08 17:46:18 +0530

    Update 4.1 - Count Partitions with Given Difference.cpp

--- Dynamic Programming/Subsequences DP/4.1 - Count Partitions with Given Difference.cpp
+++ Dynamic Programming/Subsequences DP/4.1 - Count Partitions with Given Difference.cpp
@@ -74,12 +74,8 @@ public:
 
         // Initialize the edge case: If you reached the 0th index then compute the count of all the subsets according to their existence
         dp[0][0] = 1;
-
-        if(nums[0] == 0) 
-            dp[0][0] = 2;
-
-        else if(nums[0] <= target) 
-            dp[0][nums[0]] = 1;
+        if(nums[0] == 0) dp[0][0] = 2;
+        else if(nums[0] <= target) dp[0][nums[0]] = 1;
 
         // Fill the rest of the table
         for(int index = 1; index < N; ++index) {
@@ -113,12 +109,8 @@ public:
 
         // Initialize the edge case: If you reached the 0th index then compute the count of all the subsets according to their existence
         prevRow[0] = 1;
-
-        if(nums[0] == 0) 
-            prevRow[0] = 2;
-
-        else if(nums[0] <= target) 
-            prevRow[nums[0]] = 1;
+        if(nums[0] == 0) prevRow[0] = 2;
+        else if(nums[0] <= target) prevRow[nums[0]] = 1;
 
         // Fill the rest of the table
         for(int index = 1; index < N; ++index) {
